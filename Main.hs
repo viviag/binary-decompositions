0c590e4ada75176ce9c318cf312a498de6f060db
Vitalii Guzeev <viviag@yandex.ru>
2023-05-12 02:29:09 +0200

    Types for residue fields

--- Main.hs
+++ Main.hs
@@ -1,10 +1,15 @@
+{-# LANGUAGE RecordWildCards #-}
 module Main where
+
+import System.Exit (die)
+
+import Control.Monad (when)
 
 import Options.Applicative (execParser)
 
-import Hensel.Options (options)
+import Hensel.Options (options, Options(..))
 
 main :: IO ()
 main = do
-  _opts <- execParser options
+  Options{..} <- execParser options
   return ()
