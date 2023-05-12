777602d7cd12b7e660d7393459f244877ed8d589
Vitalii Guzeev <viviag@yandex.ru>
2023-05-12 23:33:45 +0200

    Lift options to types

--- src/Hensel/Entry.hs
+++ src/Hensel/Entry.hs
@@ -1,9 +1,10 @@
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE TypeApplications #-}
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE KindSignatures #-}
 module Hensel.Entry where
 
-import GHC.TypeLits
+import GHC.TypeNats
 
 import Control.Monad (mapM_)
 
@@ -17,16 +18,17 @@ import Hensel.Algorithms.Hensel
 
 import Data.Proxy
 
-prettyPrint :: Poly (F 2) -> [Poly (R 2 2)] -> IO ()
-prettyPrint req resp = do
+prettyPrint :: (KnownNat p, KnownNat n) => Proxy p -> Proxy n -> Poly (F p) -> [Poly (R p n)] -> IO ()
+prettyPrint _ _ req resp = do
   putStrLn ("Constructed decomposition of $" <> show req <> "$ is the following:")
   putStrLn "$$"
   mapM_ (putStrLn . show) resp
   putStrLn "$$"
 
-buildPoly :: Int -> Poly (F 2)
-buildPoly degree = Poly $ [-1] ++ replicate (degree - 1) 0 ++ [1]
+buildPoly :: KnownNat p => Proxy p -> Int -> Poly (F p)
+buildPoly _ degree = Poly $ [-1] ++ replicate (degree - 1) 0 ++ [1]
 
 enter :: Options -> IO ()
-enter Options{..} = prettyPrint poly (map lift $ factorize poly)
-  where poly = buildPoly optionsDegree
+enter Options{..} = case (someNatVal optionsCharacteristic, someNatVal optionsPower) of
+  (SomeNat p, SomeNat n) -> let poly = buildPoly p optionsDegree in
+    prettyPrint p n poly (map lift $ factorize poly)
